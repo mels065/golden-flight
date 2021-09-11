b3c301d64d9f6bb0ff8218a67ab547a85f1c7ee6
Brandon Mellus <mellusbrandon@gmail.com>
2021-09-10 17:57:04 -0700

    Add handling if unexpected error happens when booking tickets

--- public/js/ticket.js
+++ public/js/ticket.js
@@ -1,23 +1,27 @@
 const ticketFormHandler = async(event) => {
   event.preventDefault();
 
-  const baggage = document.querySelector('.baggage').checked;
-  const roundtrip = document.querySelector('.roundTrip').checked;
-  const order_date = document.querySelector('.ticketDate').innerHTML;
-  const flight_id = event.target.dataset.flightid;
-  
-  const response = await fetch('/api/ticket/book', {
-    method: 'POST',
-    body: JSON.stringify({ baggage, roundtrip , order_date, flight_id}),
-    headers: { 'Content-Type': 'application/json' },
-  
-  })
+  try {
+    const baggage = document.querySelector('.baggage').checked;
+    const roundtrip = document.querySelector('.roundTrip').checked;
+    const order_date = document.querySelector('.ticketDate').innerHTML;
+    const flight_id = event.target.dataset.flightid;
+    
+    const response = await fetch('/api/ticket/book', {
+      method: 'POST',
+      body: JSON.stringify({ baggage, roundtrip , order_date, flight_id}),
+      headers: { 'Content-Type': 'application/json' },
+    
+    })
 
-  if (response.ok) {
-    // If successful, redirect the browser to the profile page
-    document.location.assign('/mytickets');
-  } else {
-    alert((await response.json()).message);
+    if (response.ok) {
+      // If successful, redirect the browser to the profile page
+      document.location.assign('/mytickets');
+    } else {
+      alert((await response.json()).message);
+    }
+  } catch (err) {
+    alert(err.message);
   }
 }
 
