9e41aeb11bf29a46f2eb118e3be5edaf297ac8a9
Kevin Shank <kevin_shank@yahoo.com>
2021-09-13 17:38:32 -0400

    make homw.handlebars responsive

--- public/js/home.js
+++ public/js/home.js
@@ -1,3 +1,16 @@
+const toggleButton = $('.toggle-button')[0]
+const navbarLinks = $('.navbar-links-dropdown')[0]
+const ticketLink = $('.my-tickets-dropdown')[0]
+const logoutLink = $('.no-button-dropdown')[0] 
+
+toggleButton.addEventListener('click', (e) => {
+ e.preventDefault();
+ 
+ ticketLink.classList.toggle('active');
+ logoutLink.classList.toggle('active');
+  
+})
+
 const updateCustomerInfo = async (event) => {
     event.preventDefault();
 
