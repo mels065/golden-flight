0af7fb50f4f484eea77f51eb8ce6200f9e5c30f6
Kevin Shank <kevin_shank@yahoo.com>
2021-09-13 19:12:27 -0400

    Make ticket.handlebars responsive

--- public/js/ticket.js
+++ public/js/ticket.js
@@ -1,3 +1,18 @@
+const toggleButton = $('.toggle-button')[0]
+const navbarLinks = $('.navbar-links-dropdown')[0]
+const ticketLink = $('.my-tickets-dropdown')[0]
+const logoutLink = $('.no-button-dropdown')[0] 
+const searchLink = $('.search-dropdown')[0]
+
+toggleButton.addEventListener('click', (e) => {
+ e.preventDefault();
+ 
+ ticketLink.classList.toggle('active');
+ logoutLink.classList.toggle('active');
+ searchLink.classList.toggle('active');
+  
+})
+
 const ticketFormHandler = async(event) => {
   event.preventDefault();
 
